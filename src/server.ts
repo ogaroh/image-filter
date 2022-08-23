7a2040b6f6e99b2993ba5c694bdc44371fca6598
Erick Ogaro <ogaroerick1998@gmail.com>
2022-08-23 15:38:24 +0300

    package update

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
+  
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
